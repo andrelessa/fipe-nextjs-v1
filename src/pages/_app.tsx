21da74338d945066521d8b29536588fd5cdbdd78
André Lessa <andre_lessa@hotmail.com>
2024-06-26 14:24:05 -0300

    Add Vercel Web Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
+import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
-
 import AppProvider from '../hooks';
-
 import GlobalStyle from '../styles/global';
-
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <AppProvider>
+      <Analytics />
       <Component {...pageProps} />
       <GlobalStyle />
     </AppProvider>
